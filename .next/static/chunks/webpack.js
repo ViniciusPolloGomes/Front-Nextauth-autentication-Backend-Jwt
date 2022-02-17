2e239783fb5322438e4243c101d68d7963a2fe07
Vinicius Pollo Gomes <ViniciusPolloGomes@gmail.com>
2022-02-16 21:06:57 -0300

    Fila de requisições no Axios

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "323cca8862d1541e"; }
+/******/ 		__webpack_require__.h = function() { return "481da45cf651ab0b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- services/api.ts
+++ services/api.ts
@@ -2,6 +2,8 @@ import axios, { AxiosError } from 'axios';
 import { parseCookies ,setCookie} from 'nookies';
 
 let cookies= parseCookies();
+let isRefreshing = false;
+let failedRequestsQueue = [];
 
 export const  api = axios.create({
     baseURL: 'http://localhost:3333',
@@ -13,28 +15,55 @@ export const  api = axios.create({
 api.interceptors.response.use(response=>{
     return response;
     }, (error: AxiosError) =>{
-        if(error.response.status === 401){
-            if(error.response.data?.code === 'token.expired'){  //renovar token
-               cookies = parseCookies(); 
-                const {'nextauth.refreshToken': refreshToken} = cookies
-                api.post('/refresh',{
-                    refreshToken,
-                }).then(response =>{
-                    const {token} = response.data;
-                   
-                    setCookie(undefined , 'nextauth.token', token,{
-                        maxAge:60*60*24*30, //1 month
-                        path: '/'
-                    })
-                    setCookie(undefined , 'nextauth.refreshToken', response.data.refreshToken, {
-                        maxAge:60*60*24*30, //1 month
-                        path: '/'
-                    })
+            if(error.response.status === 401){
+                if(error.response.data?.code === 'token.expired'){  //renovar token
+                    cookies = parseCookies(); 
+                    const {'nextauth.refreshToken': refreshToken} = cookies
+                    const originalConfig= error.config
 
-                    api.defaults.headers['Authorization'] = `Bearer ${token}`;
-                })
-            } else{                                              //deslogar usuário
-               
+                    if(!isRefreshing){
+                        isRefreshing = true;
+                        api.post('/refresh',{
+                            refreshToken,
+                        }).then(response =>{
+                                const {token} = response.data;
+                                
+                                setCookie(undefined , 'nextauth.token', token,{
+                                    maxAge:60*60*24*30, //1 month
+                                    path: '/'
+                                })
+                                setCookie(undefined , 'nextauth.refreshToken', response.data.refreshToken, {
+                                    maxAge:60*60*24*30, //1 month
+                                    path: '/'
+                                })
+            
+                                api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+                                failedRequestsQueue.forEach(request => request.onSuccess(token))
+                                failedRequestsQueue = [] ;
+                            }).catch(err =>{
+                                failedRequestsQueue.forEach(request => request.onFailure(err))
+                                failedRequestsQueue = [] ;
+                            }).finally(()=>{
+                                isRefreshing = false
+                            })
+                        
+                    }
+                    return new Promise((resolve,reject)=>{
+                        failedRequestsQueue.push({
+                            onSuccess: (token:string)=>{
+                                originalConfig.headers['Authorization'] = `Bearer ${token}`
+
+                                resolve(api(originalConfig))
+                            },
+                            onFailure: (err: AxiosError)=>{ 
+                                reject(err)
+                            }
+                        })
+                    });
+                }else{                                              //deslogar usuário
+            
+                }
             }
-        }
-    })
+        }       
+)
